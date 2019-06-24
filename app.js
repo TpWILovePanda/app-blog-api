dd577293eb38b21c262d741a70f99730824e2a9b
Terry Chen <terry@voicetube.com>
2019-06-24 09:46:17 +0800

    Fsil Test 和 註解調整

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/**
+ * 引入依賴套件
+ */
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+// require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var apiRouter = require('./routes/api');
+/**
+ * 主要
+ */
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+/**
+ * 使用引入套件
+ */
+
+// 日誌紀錄
+switch (app.get('env')) {
+  case 'development':
+    app.use(logger('dev'));
+    break;
+  case 'production':
+    const rfs = require('rotating-file-stream');
+    // create a rotating write stream
+    const accessLogStream = rfs('access.log', {
+      interval: '1d', // rotate daily
+      path: path.join(__dirname, 'log')
+    });
+    app.use(logger('combined', {
+      stream: accessLogStream
+    }));
+    break;
+  default:
+    app.use(logger('dev'));
+    break;
+}
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * 路由控制
+ */
+
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
+
+/**
+ * 暴露接口
+ */
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Fail */
+router.get('/fail', (req, res) => {
+  throw new Error('Nope!');
+});
+
+/* GET Epic Fail */
+router.get('/epic-fail', (req, res) => {
+  process.nextTick(() => {
+    throw new Error('Kaboom!');
+  });
+});
+
 module.exports = router;
